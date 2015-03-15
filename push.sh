36fd29e52f165e5748a41210a255341ea719ef80
rebelion76 <rebelion76@gmail.com>
2015-03-15 19:02:19 +0400

    изменения в push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,1 @@
 git push
-curl --cookie cookies.txt --form src=@bankiru_plus_beta.user.js --form press=Upload http://userscripts.org/scripts/upload/133598
-curl --cookie cookies.txt --form src=@bankiru_plus_beta.user.js --form press=Upload http://userscripts.org/scripts/upload/140918
